7925da4da754149bf2c825cfd1d060f7210b9182
Lucas Garcia <lucas.rita@windel.com.br>
2023-12-08 14:15:59 -0300

    FIX - Ajustes.

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,23 @@ const fs = require('fs');
 require('dotenv').config();
 
 const httpsOptions = {
-  cert: fs.readFileSync('keys/cert-chain.pem'),
-  key: fs.readFileSync('keys/key-private.pem'),
-  // cert: fs.readFileSync('keys/cert.pem'),
-  // key: fs.readFileSync('keys/privkey.pem'),
+  // cert: fs.readFileSync('keys/cert-chain.pem'),
+  // key: fs.readFileSync('keys/key-private.pem'),
+  cert: fs.readFileSync('keys/cert.pem'),
+  key: fs.readFileSync('keys/privkey.pem'),
   ca: [
-    fs.readFileSync('keys/cert1.pem'),
-    fs.readFileSync('keys/cert2.pem'),
-    fs.readFileSync('keys/cert3.pem'),
-    fs.readFileSync('keys/cert4.pem'),
+    // fs.readFileSync('keys/cert1.pem'),
+    // fs.readFileSync('keys/cert2.pem'),
+    // fs.readFileSync('keys/cert3.pem'),
+    // fs.readFileSync('keys/cert4.pem'),
+    fs.readFileSync('keys/cahomolog.pem'),
   ],
-  requestCert: true,
 };
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     httpsOptions,
+    cors: true,
   });
 
   await app.listen(process.env.NODE_PORT);
